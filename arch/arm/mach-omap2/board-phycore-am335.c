10fcd909d1408b5943624416d5c69c7df6219b91
Trilokesh Rangam <lokesh.r@phytec.in>
2013-10-01 12:25:31 +0200

    phyBOARD phyCORE Am335 Initial Support
    Signed-off-by: Trilokesh Rangam <lokesh.r@phytec.in>

---
+++ arch/arm/mach-omap2/board-phycore-am335.c
@@ -0,0 +1,212 @@
+/*
+ * Board support file for Phytec phyBOARD phyCORE Am335x Board.
+ *
+ * Copyright (C) 2013 Phytec Embedded Pvt. Ltd.
+ *
+ * Author: Trilokesh Rangam <lokesh.r@phytec.in>
+ *
+ * Based on mach-omap2/board-am335xevm.c
+ *
+ * Based on mach-omap2/board-pcm051.c
+ *
+ * Copyright (C) 2011 Texas Instruments, Inc. - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/gpio.h>
+#include <linux/input.h>
+#include <linux/platform_device.h>
+#include <linux/clk.h>
+#include <linux/err.h>
+#include <linux/export.h>
+#include <linux/ethtool.h>
+#include <linux/reboot.h>
+#include <linux/opp.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/nand.h>
+#include <linux/mtd/partitions.h>
+
+#include <mach/hardware.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/hardware/asp.h>
+
+#include <plat/omap_device.h>
+#include <plat/irqs.h>
+#include <plat/board.h>
+#include <plat/common.h>
+#include <plat/emif.h>
+#include <plat/nand.h>
+
+#include "board-flash.h"
+#include "cpuidle33xx.h"
+#include "mux.h"
+#include "hsmmc.h"
+#include "devices.h"
+
+#include "common.h"
+#include "am33xx_generic.h"
+
+/* Convert GPIO signal to GPIO pin number */
+#define GPIO_TO_PIN(bank, gpio) (32 * (bank) + (gpio))
+
+/* module pin mux structure */
+struct pinmux_config {
+	const char *string_name; /* signal name format */
+	int val; /* Options for the mux register value */
+};
+
+/*
+* @pin_mux - single module pin-mux structure which defines pin-mux
+*                       details for all its pins.
+*/
+static void setup_pin_mux(struct pinmux_config *pin_mux)
+{
+	int i;
+
+	for (i = 0; pin_mux->string_name != NULL; pin_mux++)
+		omap_mux_init_signal(pin_mux->string_name, pin_mux->val);
+
+}
+
+/* Pin mux for nand flash module */
+static struct pinmux_config nand_pin_mux[] = {
+	{"gpmc_ad0.gpmc_ad0",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad1.gpmc_ad1",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad2.gpmc_ad2",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad3.gpmc_ad3",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad4.gpmc_ad4",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad5.gpmc_ad5",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad6.gpmc_ad6",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_ad7.gpmc_ad7",     OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_wait0.gpmc_wait0", OMAP_MUX_MODE0 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_wpn.gpmc_wpn",     OMAP_MUX_MODE7 | AM33XX_PIN_INPUT_PULLUP},
+	{"gpmc_csn0.gpmc_csn0",   OMAP_MUX_MODE0 | AM33XX_PULL_DISA},
+	{"gpmc_advn_ale.gpmc_advn_ale",  OMAP_MUX_MODE0 | AM33XX_PULL_DISA},
+	{"gpmc_oen_ren.gpmc_oen_ren",    OMAP_MUX_MODE0 | AM33XX_PULL_DISA},
+	{"gpmc_wen.gpmc_wen",     OMAP_MUX_MODE0 | AM33XX_PULL_DISA},
+	{"gpmc_ben0_cle.gpmc_ben0_cle",  OMAP_MUX_MODE0 | AM33XX_PULL_DISA},
+	{NULL, 0},
+};
+
+static struct resource am33xx_cpuidle_resources[] = {
+	{
+		.start          = AM33XX_EMIF0_BASE,
+		.end            = AM33XX_EMIF0_BASE + SZ_32K - 1,
+		.flags          = IORESOURCE_MEM,
+	},
+};
+
+/* AM33XX devices support DDR2 power down */
+static struct am33xx_cpuidle_config am33xx_cpuidle_pdata = {
+	.ddr2_pdown     = 1,
+};
+
+static struct platform_device am33xx_cpuidle_device = {
+	.name                   = "cpuidle-am33xx",
+	.num_resources          = ARRAY_SIZE(am33xx_cpuidle_resources),
+	.resource               = am33xx_cpuidle_resources,
+	.dev = {
+		.platform_data  = &am33xx_cpuidle_pdata,
+	},
+};
+
+static void __init am33xx_cpuidle_init(void)
+{
+	int ret;
+
+	am33xx_cpuidle_pdata.emif_base = am33xx_get_mem_ctlr();
+
+	ret = platform_device_register(&am33xx_cpuidle_device);
+
+	if (ret)
+		pr_warning("AM33XX cpuidle registration failed\n");
+
+}
+
+static struct gpmc_timings am335x_nand_timings = {
+
+	/* granularity of 10 is sufficient because of calculations */
+	.sync_clk = 0,
+
+	.cs_on = 0,
+	.cs_rd_off = 30,
+	.cs_wr_off = 30,
+
+	.adv_on = 0,
+	.adv_rd_off = 30,
+	.adv_wr_off = 30,
+
+	.oe_on = 10,
+	.we_off = 20,
+	.oe_off = 30,
+
+	.access = 30,
+	.rd_cycle = 30,
+	.wr_cycle = 30,
+
+	.cs_cycle_delay = 50,
+	.cs_delay_en = 1,
+	.wr_access = 30,
+	.wr_data_mux_bus = 0,
+};
+
+static void am335x_nand_init(void)
+{
+	struct omap_nand_platform_data *pdata;
+	struct gpmc_devices_info gpmc_device[2] = {
+		{ NULL, 0 },
+		{ NULL, 0 },
+	};
+
+	setup_pin_mux(nand_pin_mux);
+	pdata = omap_nand_init(NULL, 0, 0, 0, &am335x_nand_timings);
+	if (!pdata)
+		return;
+	pdata->ecc_opt = OMAP_ECC_BCH8_CODE_HW;
+	pdata->elm_used = true;
+	gpmc_device[0].pdata = pdata;
+	gpmc_device[0].flag = GPMC_DEVICE_NAND;
+
+	omap_init_gpmc(gpmc_device, sizeof(gpmc_device));
+	omap_init_elm();
+}
+
+static void __init phycore_am335_init(void)
+{
+	am33xx_cpuidle_init();
+	am33xx_mux_init(NULL);
+	omap_serial_init();
+	omap_sdrc_init(NULL, NULL);
+	am335x_nand_init();
+}
+
+static void __init am335x_map_io(void)
+{
+	omap2_set_globals_am33xx();
+	omapam33xx_map_common_io();
+}
+
+MACHINE_START(PHYCORE_AM335, "phyBOARD phyCORE AM335x")
+	/* Maintainer: PHYTEC */
+	.atag_offset    = 0x100,
+	.map_io         = am335x_map_io,
+	.init_early     = am33xx_init_early,
+	.init_irq       = ti81xx_init_irq,
+	.handle_irq     = omap3_intc_handle_irq,
+	.timer          = &omap3_am33xx_timer,
+	.init_machine   = phycore_am335_init,
+MACHINE_END
